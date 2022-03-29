4ad48cc41b7f0005fcfe71b92578d44005624c50
projetogentour <102549224+projetogentour@users.noreply.github.com>
2022-03-29 09:32:10 -0300

    Add files via upload

---
+++ ProjetoGenturApplication.java
@@ -0,0 +1,13 @@
+package org.generation.gentur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGenturApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGenturApplication.class, args);
+	}
+
+}
